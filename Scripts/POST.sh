f6c610cc00d288f7d57ae7370b4fa1654ebf115b
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-12-26 14:57:48 +1300

    reduce db logging, add POST test script

---
+++ Scripts/POST.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+: "${apikey:?\$apikey variable must be provided}"
+
+i=1
+while [ $i -le 50 ]
+do
+        msg="Test message from server $i"
+    curl -X POST "http://localhost:5000/api/ServerMessage/" \
+  -H "Content-Type: application/json" \
+  -H "key: $apikey" \
+ -d @- <<EOF 
+ { 
+ "Message": "$msg" 
+ }
+EOF
+
+    ((i++))
+done
